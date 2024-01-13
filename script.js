1263f5fe901e35e9924b95074492b88ac59c7257
Boom835 <156540751+Boom835@users.noreply.github.com>
2024-01-14 00:49:42 +0300

    Update script.js
    Carousel 2

--- script.js
+++ script.js
@@ -71,3 +71,17 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 4 seconds
+}
