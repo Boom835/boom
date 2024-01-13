9096b884a8b0bdafdbbe7f1c9f2f8d549ef672d2
Boom835 <156540751+Boom835@users.noreply.github.com>
2024-01-14 01:02:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,6 @@ function submitOrder() {
   window.location.href = mailtoLink;
 }
 
-
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -56,16 +55,18 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-function showSlides() {
+function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 4 seconds
+  dots[slideIndex-1].className += " active";
 }
-let slideIndex = 0;
-showSlides();
